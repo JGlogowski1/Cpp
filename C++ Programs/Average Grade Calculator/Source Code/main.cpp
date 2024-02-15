f5ceed1c34c8d87a094cb484067d9ddc9668583b
JGlogowski1 <160066286+JGlogowski1@users.noreply.github.com>
2024-02-14 22:39:04 -0400

    Update main.cpp
    Added comments

--- C++ Programs/Average Grade Calculator/Source Code/main.cpp
+++ C++ Programs/Average Grade Calculator/Source Code/main.cpp
@@ -2,37 +2,39 @@
 #include <vector>
 
 int main() {
-
-    std::vector<int> gradeArr;
-    int sum;
-    int grade;
+    std::vector<int> gradeArr; // Vector to store grades
+    int sum = 0; // Variable to store the sum of grades
+    int grade; // Variable to store each grade entered by the user
 
     do {
-        //asks user for input of grades
-        std::cout << "Input grades and when done, input -1:";
+        // Asks the user for input of grades until -1 is entered
+        std::cout << "Input grades, and when done, input -1: ";
         std::cin >> grade;
         std::cout << std::endl;
 
-        if(grade != -1) {
-
-            //adds grade to sum
+        if (grade != -1) {
+            // Adds the grade to the sum and pushes it into the vector
             sum += grade;
-
             gradeArr.push_back(grade);
         }
 
-    }while(grade != -1);
+    } while (grade != -1);
 
+    // Displays the total number of grades entered
     std::cout << std::endl << "You entered: " << gradeArr.size() << " grades." << std::endl;
 
-    //prints all the grades
+    // Prints all the grades entered
     std::cout << "Grades entered: ";
-    for (int i : gradeArr){
+    for (int i : gradeArr) {
         std::cout << i << " ";
     }
 
-    //prints the average
-    std::cout << std::endl << "The average grade is: " << (sum / gradeArr.size()) << std::endl;
+    // Calculates and displays the average grade
+    if (!gradeArr.empty()) {
+        std::cout << std::endl << "The average grade is: " << (sum / gradeArr.size()) << std::endl;
+    } else {
+        std::cout << std::endl << "No grades entered. Average cannot be calculated." << std::endl;
+    }
 
     return 0;
 }
